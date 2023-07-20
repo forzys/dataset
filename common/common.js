77e2b111e0e42ba91f2451761c660f7b511ecb8a
forzys <zys@outlook.in>
2023-07-20 09:42:59 +0800

    move file to common

---
+++ common/common.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path')
+
+
+const dateFormat = function(input) { 
+    const date = new Date( input || Date.now())
+    const formatType = {
+        Y: date.getFullYear(),
+        M: date.getMonth() + 1,
+        D: date.getDate(),
+        h: date.getHours(),
+        m: date.getMinutes(),
+        s: date.getSeconds(),
+    }
+    
+      return {
+          format:(formatStr)=>{
+            return formatStr ? formatStr.replace(
+                /Y+|M+|D+|h+|m+|s+/g,
+                target => (new Array(target.length).join('0') + formatType[target[0]]).substr(-target.length)
+            ): date.getTime()
+          }
+      }
+} 
+
+
+function jsonFormat(text='', init={}) {
+    try{
+        const isJson = /\[]|{}/.test(text.replace(/(?<=.).*(?=.$)/,''))
+        return isJson ? JSON.parse(text) : init
+    }catch(e){
+        return init
+    }
+}
+
+const createFile = function(file, data){
+    function createDir (name) {
+        if (fs.existsSync(name)) {
+            return true
+        }
+
+        if (createDir(path.dirname(name))) {
+            fs.mkdirSync(name)
+            return true
+        }
+    }
+ 
+    // const files = fs.readdirSync(externalDir); 
+
+    if (createDir(path.dirname(file)) && !!data) {
+        fs.writeFileSync(file, data) 
+    }
+}
+
+const readFile = function(file){
+    try{
+        createFile(file, '')
+        return fs.readFileSync(file).toString();
+    }catch(e){
+        return ''
+    }
+}
+
+
+exports.readFile = readFile
+exports.jsonFormat = jsonFormat
+exports.dateFormat = dateFormat
+exports.createFile = createFile
